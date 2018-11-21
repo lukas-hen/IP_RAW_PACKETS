20629f35445c0d9ff876641e086c9ebcea7b64f5
Lukas <lukashen@kth.se>
2018-11-21 10:26:27 +0100

    Updated README & Comments

--- packet_maker.c
+++ packet_maker.c
@@ -1,7 +1,7 @@
 #include "packet_maker.h"
 
 #define MAX_CMP_WORDS 15
-#define DG_BUFFER 4096
+#define DG_BUFFER 4076
 
 
 
@@ -24,7 +24,8 @@ int main(int argc, char * argv[]){
 
 	// Make Buffer for datagram and cast to the IP_header struct (defined in packet_maker.h).
 	char datagram[DG_BUFFER];
-	char payload_buffer[DG_BUFFER-20]; // Assuming header is always 20 bytes.
+	// Following line assumes header is always 20 bytes. Note that i am referring to payload which in this scope would include headers from higher level protocols of the OSI stack.
+	char payload_buffer[DG_BUFFER-20];
 	struct IP_header * iph = (struct IP_header *) datagram;
 
 	//Zero the buffer.
@@ -37,17 +38,17 @@ int main(int argc, char * argv[]){
 
 	//Set values of IP header. See rfc791 for meaning of all values.
 	//Also sets default fallback values if flags are not specified for a specific header field.
-	iph->tos = 0;					// 0 is normal service. 
-	iph->ihl = nwords;				// 5 is default header without options.
-	iph->version = 4;				// 4 is ipv4, Ipv6 not implemented.
-	iph->total_len = 20;			// 20 bytes if no payload
-	iph->identification = 65535;	// Max value for recognization
-	iph->flags = 3;					// 3 bits 0b010 (Do not fragment, and no more fragments.)
-	iph->offset = 0;				// No offset unless fragmentation.
-	iph->ttl = 128;					// 8 bits 0x80 = 128
-	iph->protocol = 4;				// 4 for ipv4 
-	iph->src_address = inet_addr("127.0.0.1");	// 32 bits spoofed. 123.123.123.123
-	iph->dst_address = inet_addr("255.255.255.255");	// 32 bits localhost: 127.0.0.1
+	iph->tos = 0;		// 0 is normal service. 
+	iph->ihl = nwords;		// 5 is default header without options.
+	iph->version = 4;		// 4 is ipv4, Ipv6 not implemented.
+	iph->total_len = 20;		// 20 bytes if just header
+	iph->identification = 65535;		// Max value for recognization
+	iph->flags = 3;		// 3 bits 0b010 (Do not fragment, and no more fragments.)
+	iph->offset = 0;		// No offset unless fragmentation.
+	iph->ttl = 128;		// 8 bits 0x80 = 128
+	iph->protocol = 4;		// 4 for ipv4 
+	iph->src_address = inet_addr("127.0.0.1");		// 32 bits, fallback
+	iph->dst_address = inet_addr("127.0.0.1");		// 32 bits, fallback to localhost
 
 	//Update header based on options.
 	iph = parse_options_and_modify_struct(argc, argv, iph);
@@ -76,7 +77,7 @@ int main(int argc, char * argv[]){
 	sin.sin_family = PF_INET;
 	sin.sin_addr.s_addr = iph->src_address; // Can be set to your interface ip with src_address being somethin else if you want to forge packets.
 
-	//Read payload from stdin
+	//Read data to be sent from stdin (Data will have to be wrapped in higher level protocol)
 	read(STDIN_FILENO, payload_buffer, sizeof(payload_buffer));
 
 	//Append to buffer after header.
